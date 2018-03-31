5b8c01d7d1abfc13f57a765985bc49dd8d63d579
Chris Henry <cdhenry@gmail.com>
2018-03-31 12:45:58 +0000

    automatically backed up by learn

--- db/migrate/20180331124533_create_passenger_taxis.rb
+++ db/migrate/20180331124533_create_passenger_taxis.rb
@@ -1,6 +1,8 @@
 class CreatePassengerTaxis < ActiveRecord::Migration
   def change
     create_table :passenger_taxis do |t|
+      t.belongs_to :passenger
+      t.belongs_to :taxi
     end
   end
 end
